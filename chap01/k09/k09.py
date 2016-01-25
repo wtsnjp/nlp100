426f1640db30aad8419bea30762244a1a50b9a06
Watson <wtsnjp@gmail.com>
2016-01-25 21:53:09 +0900

    solve k09

---
+++ chap01/k09/k09.py
@@ -0,0 +1,20 @@
+import random
+
+str = """\
+I couldn't believe that I could actually understand what I was reading :
+the phenomenal power of the human mind.
+"""
+words = str.replace('.', '').replace(',', '').split()
+
+for i in range(len(words)):
+    w = list(words[i])
+    l = len(w)
+    if l > 3:
+        while True:
+            x, y = random.randint(1, l-2), random.randint(1, l-2)
+            if x == y:
+                break
+            w[x], w[y] = w[y], w[x]
+    words[i] = ''.join(w)
+
+print(' '.join(words))
