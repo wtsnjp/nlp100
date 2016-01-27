c2e8bfec04cd350c1ddd0e9e0b071c73b03d7657
Watson <wtsnjp@gmail.com>
2016-01-27 17:55:13 +0900

    clean k23

--- chap03/k23/k23.py
+++ chap03/k23/k23.py
@@ -6,6 +6,6 @@ file_name = sys.argv[1]
 with open(file_name) as f:
     for l in f:
         r = re.compile("(=+)(.*?)=+")
-        if r.match(l):
-            m = r.match(l)
+        m = r.match(l)
+        if m:
             print(m.group(2).strip(), len(m.group(1))-1)
