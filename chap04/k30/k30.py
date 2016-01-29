1e8928d19db0f1dd5b17c7d21098e52db06ef2d4
Watson <wtsnjp@gmail.com>
2016-01-29 13:28:59 +0900

    solve k30

---
+++ chap04/k30/k30.py
@@ -0,0 +1,21 @@
+import sys
+
+file_name = sys.argv[1]
+
+sentences = []
+
+with open(file_name) as f:
+    s = []
+    for l in f:
+        if l == "EOS\n":
+            if s != []:
+                sentences.append(s)
+                s = []
+        else:
+            l = l.replace('\t', ',')
+            v = l.split(',')
+            s.append({'surface': v[0], 'base': v[7], 'pos': v[1], 'pos1': v[2]})
+
+# print(sentences)
+for s in sentences:
+    print(s)
