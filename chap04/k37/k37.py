cb8dff3a9f94c42bf24496e05f2c674d389ca9d6
Watson <wtsnjp@gmail.com>
2016-02-01 19:23:06 +0900

    solve k37

---
+++ chap04/k37/k37.py
@@ -0,0 +1,44 @@
+import sys
+import collections
+from matplotlib import pyplot as plt
+
+NUM = 10
+
+file_name = sys.argv[1]
+
+sentences = []
+
+with open(file_name) as f:
+    s = []
+    for l in f:
+        if l == "EOS\n":
+            if s != []:
+                sentences.append(s)
+                s = []
+        else:
+            l = l.replace('\t', ',')
+            v = l.split(',')
+            s.append({'surface': v[0], 'base': v[7], 'pos': v[1], 'pos1': v[2]})
+
+tango = []
+
+for s in sentences:
+    for v in s:
+        tango.append(v['base'])
+
+cnt = 0
+k_list = []
+v_list = []
+d_list = range(1, NUM+1)
+
+count_dict = collections.Counter(tango)
+for k,v in count_dict.most_common():
+    k_list.append(k)
+    v_list.append(v)
+    cnt += 1
+    if cnt == NUM:
+        break
+
+plt.bar(d_list, v_list, align="center")
+plt.xticks(d_list, k_list)
+plt.savefig("result.png")
