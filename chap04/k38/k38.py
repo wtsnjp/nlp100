01362b6b3bb5e3114970dc04ec0ea8dbb18c2083
Watson <wtsnjp@gmail.com>
2016-02-01 23:39:23 +0900

    solve k38

---
+++ chap04/k38/k38.py
@@ -0,0 +1,38 @@
+import sys
+import collections
+from matplotlib import pyplot as plt
+
+NUM = 10
+
+file_name = sys.argv[1]
+
+sentences = []
+
+with open(file_name) as f:
+    s = []
+    for l in f:
+        if l == "EOS\n":
+            if s != []:
+                sentences.append(s)
+                s = []
+        else:
+            l = l.replace('\t', ',')
+            v = l.split(',')
+            s.append({'surface': v[0], 'base': v[7], 'pos': v[1], 'pos1': v[2]})
+
+tango = []
+
+for s in sentences:
+    for v in s:
+        tango.append(v['base'])
+
+count_dict = collections.Counter(tango)
+count_list = []
+
+for k,v in count_dict.most_common():
+    count_list.append(v/len(count_dict))
+
+plt.hist(count_list, 50, range=(0.0, 0.01))
+plt.xlabel("出現頻度")
+plt.ylabel("種類数")
+plt.savefig("result.png")
