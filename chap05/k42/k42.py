df80559947fd2b72347ab65a0debf65d3b08c487
Watson <wtsnjp@gmail.com>
2016-02-08 12:47:07 +0900

    solve k42

---
+++ chap05/k42/k42.py
@@ -0,0 +1,62 @@
+import sys
+
+class Morph:
+    def __init__(self, surface, base, pos, pos1):
+        self.surface = surface
+        self.base    = base
+        self.pos     = pos
+        self.pos1    = pos1
+
+class Chunk:
+    def __init__(self, morphs, dst):
+        self.morphs = morphs
+        self.dst    = dst
+        self.srcs   = []
+
+file_name = sys.argv[1]
+sentences, phrases = [], []
+
+with open(file_name) as f:
+    s, p = [], []
+    for l in f:
+        if l == "EOS\n":
+            if p != []:
+                c = Chunk(p, dst)
+                phrases.append(c)
+                p = []
+            if phrases != []:
+                sentences.append(phrases)
+                phrases = []
+        elif l[0] == "*":
+            if p != []:
+                c = Chunk(p, dst)
+                phrases.append(c)
+                p = []
+            l_list = l.split(" ")
+            dst = int(l_list[2][:-1])
+        else:
+            l = l.replace('\t', ',')
+            v = l.split(',')
+            if len(v) == 10:
+                m = Morph(v[0], v[7], v[1], v[2])
+                p.append(m)
+
+for s in sentences:
+    n = 0
+    for c in s:
+        if c.dst != -1 and c.dst < len(s):
+            s[c.dst].srcs.append(n)
+        n += 1
+
+for s in sentences:
+    for c in s:
+        if c.dst != -1 and c.dst < len(s):
+            a, b = "", ""
+            for v in c.morphs:
+                if v.pos != "記号":
+                    a += v.surface
+            for v in s[c.dst].morphs:
+                if v.pos != "記号":
+                    b += v.surface
+            if a != "" and b != "":
+                print(a, b, sep="\t")
