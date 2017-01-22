c302bd30082b10c5ddb5ee3f00c9d336d0938a6b
Watson <wtsnjp@gmail.com>
2017-01-22 16:28:14 +0900

    add make_data.sh

---
+++ chap02/make_data.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget "http://www.cl.ecei.tohoku.ac.jp/nlp100/data/hightemp.txt"
---
+++ chap04/make_data.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget "http://www.cl.ecei.tohoku.ac.jp/nlp100/data/neko.txt"
+mecab neko.txt > neko.txt.mecab
+rm neko.txt
