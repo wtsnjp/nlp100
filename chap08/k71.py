b67f2c4552829dce6444162ea0009575ee8938d9
Watson <wtsnjp@gmail.com>
2017-03-04 23:14:18 +0900

    clean k71

--- chap08/k71.py
+++ chap08/k71.py
@@ -4,17 +4,13 @@
 
 import sys
 from collections import Counter
-from bisect import bisect_left
 
 def stopwords(sl, fr):
     c, st = Counter([w for s in sl for w in list(set(s))]), len(sl) * fr / 100
-    return sorted([k for k, v in c.items() if v > st])
+    return [k for k, v in c.items() if v > st]
 
 def is_stopword(w, ss):
-    i, b = bisect_left(ss, w), False
-    if i != len(ss) and ss[i] == w:
-        b = True
-    return b
+    return w in ss
 
 if __name__ == '__main__':
     fn = sys.argv[1]
