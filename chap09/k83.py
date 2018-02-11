217ccc7d6161da20ecc15a17f45bd1f85fa9e64b
Watson <wtsnjp@gmail.com>
2018-02-11 19:39:33 +0900

    update k83

--- chap09/k83.py
+++ chap09/k83.py
@@ -3,45 +3,74 @@
 #
 
 import sys
-import random
-
-def sum_tc(fn, t, c):
-    s = 0
-    for l in open(fn):
-        tmp = list(map(lambda s: s.strip(), l.split('\t')))
-        if len(tmp) == 2:
-            if tmp[0] == t and tmp[1] == c:
-                s += 1
-    return s
-
-def sum_t(fn, t):
-    s = 0
-    for l in open(fn):
-        tmp = list(map(lambda s: s.strip(), l.split('\t')))
-        if len(tmp) == 2:
-            if tmp[0] == t:
-                s += 1
-    return s
-
-def sum_c(fn, c):
-    s = 0
-    for l in open(fn):
-        tmp = list(map(lambda s: s.strip(), l.split('\t')))
-        if len(tmp) == 2:
-            if tmp[1] == c:
-                s += 1
-    return s
+import plyvel
 
 def count_context(fn):
+    co_db = plyvel.DB('./co.ldb', create_if_missing=True)
+    word_db = plyvel.DB('./word.ldb', create_if_missing=True)
+    context_db = plyvel.DB('./context.ldb', create_if_missing=True)
+
     s = 0
+    ZERO = s.to_bytes((s.bit_length() + 7) // 8, 'big')
+    
     for l in open(fn):
+        tmp = list(map(lambda s: s.strip(), l.split('\t')))
+        if len(tmp) != 2:
+            continue
+
+        # f(t, c)
+        t_key = '\t'.join(tmp).encode('utf-8')
+        i = int.from_bytes(co_db.get(t_key, ZERO), 'big') + 1
+        co_db.put(t_key, i.to_bytes((i.bit_length() + 7) // 8, 'big'))
+
+        # f(t, *)
+        t_key = tmp[0].encode('utf-8')
+        i = int.from_bytes(word_db.get(t_key, ZERO), 'big') + 1
+        word_db.put(t_key, i.to_bytes((i.bit_length() + 7) // 8, 'big'))
+
+        # f(*, c)
+        t_key = tmp[1].encode('utf-8')
+        i = int.from_bytes(context_db.get(t_key, ZERO), 'big') + 1
+        context_db.put(t_key, i.to_bytes((i.bit_length() + 7) // 8, 'big'))
+
+        # N
         s += 1
+
+    co_db.close()
+    word_db.close()
+    context_db.close()
+
     return s
+
+def show_result(t, c):
+    i = 0
+    ZERO = i.to_bytes((i.bit_length() + 7) // 8, 'big')
+
+    if c == '*':
+        word_db = plyvel.DB('./word.ldb', create_if_missing=True)
+        t_key = t.encode('utf-8')
+        i = int.from_bytes(word_db.get(t_key, ZERO), 'big')
+        print('f("{}", *) = {}'.format(t, i))
+        word_db.close()
+    elif t == '*':
+        context_db = plyvel.DB('./context.ldb', create_if_missing=True)
+        t_key = c.encode('utf-8')
+        i = int.from_bytes(context_db.get(t_key, ZERO), 'big')
+        print('f(*, "{}") = {}'.format(c, i))
+        context_db.close()
+    else:
+        co_db = plyvel.DB('./co.ldb', create_if_missing=True)
+        t_key = '\t'.join((t, c)).encode('utf-8')
+        i = int.from_bytes(co_db.get(t_key, ZERO), 'big')
+        print('f("{}", "{}") = {}'.format(t, c, i))
+        co_db.close()
 
 if __name__ == '__main__':
     fn = sys.argv[1]
 
-    print('f("is", "a") = {}'.format(sum_tc(fn, 'is', 'a')))
-    print('f("is", *) = {}'.format(sum_t(fn, 'is')))
-    print('f(*, "a") = {}'.format(sum_c(fn, 'a')))
-    print('N = {}'.format(count_context(fn)))
+    s = count_context(fn)
+
+    show_result('of', 'a')
+    show_result('of', '*')
+    show_result('*', 'a')
+    print('N = {}'.format(s))
