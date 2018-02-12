5223846786b70dd9c198f98f7a620e70b40fab3d
Watson <wtsnjp@gmail.com>
2018-02-12 13:55:54 +0900

    update k84

--- chap09/k84.py
+++ chap09/k84.py
@@ -4,15 +4,14 @@
 
 import sys
 import plyvel
+import struct
 from math import log
 
-def wc_matrix(n, ofn):
+def create_matrix(n):
     co_db = plyvel.DB('./co.ldb', create_if_missing=True)
     word_db = plyvel.DB('./word.ldb', create_if_missing=True)
     context_db = plyvel.DB('./context.ldb', create_if_missing=True)
-
-    x = 0
-    ZERO = x.to_bytes((x.bit_length() + 7) // 8, 'big')
+    matrix_db = plyvel.DB('./matrix.ldb', create_if_missing=True)
 
     for k, v in co_db:
         tmp = k.decode('utf-8').strip().split('\t')
@@ -23,20 +22,28 @@ def wc_matrix(n, ofn):
 
         f_tc = int.from_bytes(v, 'big')
         if f_tc >= 10:
-            f_t = int.from_bytes(word_db.get(tmp[0].encode('utf-8'), ZERO), 'big')
-            f_c = int.from_bytes(context_db.get(tmp[1].encode('utf-8'), ZERO), 'big')
+            f_t = int.from_bytes(word_db.get(tmp[0].encode('utf-8')), 'big')
+            f_c = int.from_bytes(context_db.get(tmp[1].encode('utf-8')), 'big')
             x = max(log(2, n * f_tc / (f_t * f_c)), 0)
 
         if x != 0:
-            with open(ofn, 'a') as f:
-                f.write('{}\t{}\t{}\n'.format(tmp[0], tmp[1], x))
+            matrix_db.put(k, struct.pack('>d', x))
 
     co_db.close()
     word_db.close()
     context_db.close()
+    matrix_db.close()
+
+def get_matrix(t, c):
+    matrix_db = plyvel.DB('./matrix.ldb', create_if_missing=True)
+    t_key = '\t'.join((t, c)).encode('utf-8')
+    v = float(struct.unpack('>d', matrix_db.get(t_key))[0])
+    matrix_db.close()
+
+    print('X("{}", "{}") = {}'.format(t, c, v))
 
 if __name__ == '__main__':
     N = int(sys.argv[1])
-    ofn = 'wc-matrix.txt'
 
-    wc_matrix(N, ofn)
+    create_matrix(N)
+    get_matrix('of', 'a')
